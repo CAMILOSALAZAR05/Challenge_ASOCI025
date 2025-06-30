66caf5f6959455fa5aa27c5ec281be529886cb3d
CAMILOSALAZAR05 <juan.salazar.messa@correounivalle.edu.co>
2025-06-30 15:25:35 -0500

    main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,14 @@
-import sys
-import os
-
-# Añadir la carpeta raíz al path para imports relativos
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from src.data.loader import cargar_instancia
-from src.model.optimizer import resolver_instancia # type: ignore
-from src.visualization.display import mostrar_resultado
+from pathlib import Path
+from src.optimizer.model.model import resolver_instancia
+from src.utils.loader import cargar_instancia_desde_menu, cargar_instancia_personalizada
+from src.utils.display import mostrar_resultado
 
 def main():
+    ruta = Path(__file__).resolve().parent.parent / "data"
     while True:
         print("\n MENÚ DE OPCIONES ")
         for i in range(1, 11):
-            print(f"  {i}. Resolver Instance{i}.json")
+            print(f"  {i}. Resolver instance{i}.json")
         print("  A. Agregar nueva instancia desde otra ruta")
         print("  0. Salir")
 
@@ -21,26 +17,21 @@ def main():
         if seleccion == "0":
             print(" ¡Hasta luego!")
             break
-
         elif seleccion == "A":
             path = input(" Escribe la ruta completa del archivo .json: ").strip()
             try:
-                import json
-                with open(path, "r", encoding="utf-8") as f:
-                    datos = json.load(f)
-                    resultado = resolver_instancia(datos)
-                    mostrar_resultado(f"Instancia personalizada ({path})", resultado)
+                datos = cargar_instancia_personalizada(path)
+                resultado = resolver_instancia(datos)
+                mostrar_resultado(f"Instancia personalizada ({path})", resultado)
             except Exception as e:
                 print(f" Error al cargar la instancia: {e}")
-
         elif seleccion.isdigit() and 1 <= int(seleccion) <= 10:
             try:
-                from src.data.loader import cargar_instancia
-                datos = cargar_instancia(f"Instance{seleccion}.json")
+                datos = cargar_instancia_desde_menu(seleccion, ruta)
                 resultado = resolver_instancia(datos)
                 mostrar_resultado(f"Instance {seleccion}", resultado)
-            except FileNotFoundError as e:
-                print(e)
+            except FileNotFoundError:
+                print(f" El archivo instance{seleccion}.json no se encontró.")
         else:
             print(" Opción inválida. Intenta de nuevo.")
 
